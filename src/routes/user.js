769fcf7c12a0ffcb2886e8bb56ddd0c970ff4ab8
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-01-19 14:46:15 +0100

    add get user routes and generate/token routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,18 @@
 const router = require("express").Router()
+const { auth } = require("../middlewares/auth")
 const controllers = require("./../controllers/user")
 
 
 
+const all = [auth]
 
 
 router.post("/create/superadmin", controllers.createSuperAdmin)
+router.post("/login", controllers.login)
+
+// auth endpoints 
+router.get("/",...all, controllers.getUser)
+router.post("/generate/token",  controllers.generateAccessToken)
 
 
 
