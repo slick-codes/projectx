facd40f41416c58a0040519681cde14561935773
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-01-19 14:45:44 +0100

    add getUser, login and a few other controllers

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,6 +3,7 @@ const CustomError = require("../helpers/error")
 const User = require("../models/User")
 const Group = require("../models/Group")
 const { OK } = require("http-status-codes")
+const Token = require("../models/Token")
 
 
 
@@ -32,7 +33,7 @@ module.exports.createSuperAdmin = async function(req, res, next){
          group: group.id
       })
 
-      user.encrypt("password")
+      await user.encrypt("password")
       await user.save()
 
       res.status(OK).json({
@@ -42,18 +43,100 @@ module.exports.createSuperAdmin = async function(req, res, next){
          data: user
       })
 
-
    }catch(error){
       return next({error})
    }
 }
 
 
-
-module.exports.createAccount = async function(req,res,next){
+module.exports.login = async function(req,res,next){
    try{
+        const schema = new Schema({
+            email: { type: 'email', required: true },
+            password: { type: 'string', required: true },
+        })
+
+        const result = schema.validate(req.body)
+        if (result.error) {
+            return next(CustomError.badRequest('Invalid request body', result.error))
+        }
+
+        const body = result.data
+        const user = await User.verifyLoginCredentials(body)
+        if (user instanceof CustomError) return next(user)
+
+        const tokens = await Token.generate(user)
+        if (tokens instanceof CustomError) {
+            return next(tokens)
+        }
+        res.status(OK).json({
+            success: true,
+            status: res.statusCode,
+            message: 'User login successfully',
+            data: {
+                user,
+                tokens,
+            },
+        })
+   }catch(error){
+      return next({error})
+   }
+}
+
+
+module.exports.getUser = async function(req,res,next){
+   try{
+      res.status(OK).json({
+         success: true,
+         status: res.statusCode,
+         message: "User's info fetched successfully",
+         data: {
+                ...req.user.dataValues,
+                group: req.group || {},
+         },
+      })
+   }catch(error){
+      return next({error})
+   }
+}
+
+
+module.exports.generateAccessToken = async function(req, res, next){
+    try {
+        const schema = new Schema({ refreshToken: { type: 'string', required: true } })
+        const result = schema.validate(req.body)
+        if (result.error) {
+            return next(CustomError.badRequest('Invalid request body', result.error))
+        }
+
+        const body = result.data
+
+        const tokens = await Token.generateNewAccessToken(body.refreshToken)
+        if (tokens instanceof CustomError) {
+            return next(tokens)
+        }
+
+        res.status(OK).json({
+            success: true,
+            status: res.statusCode,
+            data: tokens,
+        })
+    } catch (error) {
+        return next({ error })
+    }
+}
+
+
+module.exports.createAccount = async function(req, res, next){
+   try{
+      const schema = new Schema({
+      })
 
    }catch(error){
       return next({error})
    }
 }
+
+
+
+
