2b6c51893169e485ce13b2179c49257a32c5e8a4
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-01-19 12:11:02 +0100

    import new files and routes

--- src/app.js
+++ src/app.js
@@ -3,16 +3,23 @@ const http = require("http")
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
-
+const { NOT_FOUND } = require("http-status-codes")
 
 
 require("dotenv").config() // setup enviroment variables
+
+const { generateSuperAdminGroupAndRole } = require("./services/services")
+
 require("./config/redis") // setup redis server connection
 require("./config/db").startDB([
    // add codes that will run when database connection is completed
+   generateSuperAdminGroupAndRole
 ]) // setup mysql database connection
 
 
+// import middlewares and routes
+const errorMiddleware = require("./middlewares/error")
+const routes = require("./routes")
 
 
 const app = express()
@@ -25,6 +32,21 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use("/static", express.static(path.join(__dirname, "..", "static")))
+
+
+app.use("/api/v1/users", routes.user)
+
+// error middleware
+app.use(errorMiddleware)
+// handle 404
+app.use(function (_req, res, _next) {
+    return res.status(NOT_FOUND).json({
+        message: 'Resources not found!',
+        success: false,
+        status: res.statusCode,
+        data: null,
+    })
+})
 
 
 
