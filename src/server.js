a00d4086876d2a4f7399e5c818a624bf279a54a3
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-01-19 10:04:39 +0100

    setup the server file

---
+++ src/server.js
@@ -0,0 +1,26 @@
+
+const server = require("./app")
+
+
+const PORT = process.env.PORT || 5004
+const listener = server.listen(PORT, _ => {
+    console.error()
+    console.error("-------------------------------------")
+    console.error(`App is running on port ${PORT}`)
+    console.error("-------------------------------------")
+})
+
+process.on("uncaughtException", error => {
+    console.error("UNCAUGHT EXCEPTION! Shutting down...")
+    console.error(error.name, error.message)
+    console.log(error)
+})
+
+process.on("unhandledRejection", error => {
+    console.error("UNHANDLED REJECTION! Shutting down...")
+    console.error(error.name, error.message)
+    console.log(error)
+
+    listener.close(_ => process.exit(1))
+})
+
